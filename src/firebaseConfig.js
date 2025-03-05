10503211b2f24be8846e269f11fc9182735fdf8b
Piyush Baburaj Puthanpurakkal <68910350+Piyush-Puthenpurakkal@users.noreply.github.com>
2025-03-05 14:21:53 +0530

    Update firebaseConfig.js
    This is just a sample Firebase config file. Try using your own auth provider.

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAHfyqNQ2RKTXd5UaTyUGKz3XCmq6kMavM",
-  authDomain: "mini-link-management.firebaseapp.com",
-  projectId: "mini-link-management",
-  storageBucket: "mini-link-management.firebasestorage.app",
-  messagingSenderId: "882848961869",
-  appId: "1:882848961869:web:eadd0f2bf91e8dc1f8d304",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
