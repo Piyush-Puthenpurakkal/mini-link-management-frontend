739b0088a10791dded1339f2506a180846559124
Piyush Baburaj <piyushbaburj98@gmail.com>
2025-03-05 23:23:20 +0530

    backend render link

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mini-link-management-backend.onrender.com",
 });
 
 // Automatically add Authorization token to every request
